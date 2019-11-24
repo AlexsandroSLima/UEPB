258467b15d9ccb12b287f2f72ddad4e7b1c5d37e
AlexsandroSLima <48229299+AlexsandroSLima@users.noreply.github.com>
2019-11-23 23:58:35 -0300

    Update lab7.c

--- labs/lab7.c
+++ labs/lab7.c
@@ -1,238 +1,215 @@
+//laboratorio 7 pilhas encadeadas
+//Alexsandro da Silva Lima
+
 #include <stdio.h>
 #include <windows.h>
 #include <conio.h>
 #include <stdlib.h>
 
-
-void gotoxy(int cord_x, int cord_y) {
+void gotoxy(int cordX, int cordY){
 
    COORD c;
-   c.X = cord_x;
-   c.Y = cord_y;
-
+   c.X = cordX;
+   c.Y = cordY;
    SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), c);
 }
-
+//struct da pilha
 typedef struct pilha{
  int top;
  int discos[3];
 } Pilha;
-
+//criando pilha
 Pilha hannoi[3];
-
-
-//Inicializando
-void inicializa (Pilha *pilha){
-
- (*pilha).top=-1;
-
+//limpa tela
+void limpaTela(){
+    system("cls");
 }
-
-//Empilhando
+//pausa tela
+void pausa(){
+    system("pause");
+}
+//limpa buffer
+void limpaBuffer(){
+    setbuf(stdin,NULL);
+}
+//Inicializa
+void inicializa(Pilha *pilha){
+   (*pilha).top=-1;
+}
+//Empilha
 void push(Pilha *pilha, int disco){
-
-  (*pilha).discos[++(*pilha).top]=disco;
+   (*pilha).discos[++(*pilha).top]=disco;
 }
-
-
-//Desempilhando
+//Desempilha
 int pop(Pilha *pilha){
-
-  return (*pilha).discos[ ((*pilha).top)-- ];
-
+  return (*pilha).discos[((*pilha).top)--];
 }
-
 //Verifica topo
-int stacktop(Pilha pilha){
-
+int pilhaSuperior(Pilha pilha){
    return pilha.discos[pilha.top];
 }
 //Retorna se cheia
 int underflow(Pilha pilha){
-  return pilha.top == 3-1;
+   return pilha.top == 3-1;
 }
-
-int empty(Pilha pilha){
-
-   return pilha.top==-1;
+int empilha(Pilha pilha){
+   return pilha.top == -1;
 }
-
 //Funcoes do Jogo
-start(Pilha hannoi[3]){
-
-int i,j;
-
-  //Empilhando....
+inicio(Pilha hannoi[3]){
+  int i= 0,j = 0;
+  //Empilha
   push(hannoi,3);
   push(hannoi,2);
   push(hannoi,1);
 
-  for(j=1;j<3;j++){
+  for(j=1;j<3;j++)
     for(i=0;i<3;i++){
        hannoi[j].discos[i]=0;
        }
-    }
 }
 
-end(Pilha hannoi[3]){
-
-   if ( (hannoi[2].discos[0]==3) && (hannoi[2].discos[1]==2) && (hannoi[2].discos[2]==1) ){
+fim(Pilha hannoi[3]){
+   if((hannoi[2].discos[0]==3) && (hannoi[2].discos[1]==2) && (hannoi[2].discos[2]==1) ){
        return 1;
-   }else{
+   }else
        return 0;
    }
 
-}
 
 imprime(Pilha hannoi[3]){
 
-    char bloco=-37;
-    int i,j=0;
-    int cord=4;
-    gotoxy(30,0);
-    printf("TORRE DE HANOI \n \n ");
-    gotoxy(0,2);
-    printf("PILHA 1");
+    char caracter = 254;
+    int i = 0,j = 0, cord = 4;
+    gotoxy(30,0);printf(" \t\tTorre de HANOI ");
+    gotoxy(30,0.5);printf("\n*****************************************************************************************************");
+    gotoxy(30,2.5);printf("\n-----------------------------------------------------------------------------------------------------");
+    gotoxy(0,2);printf("  Pilha 1");
     gotoxy(0,cord++);
-    for (j=hannoi[0].top; j>=0; j--){
-        if ( hannoi[0].discos[j]== 1){
-         printf("   %c%c%c ",bloco, bloco, bloco);
-          gotoxy(0,cord++);
-        }else if( hannoi[0].discos[j]== 2){
-           printf("  %c%c%c%c%c",bloco, bloco, bloco, bloco, bloco);
-            gotoxy(0,cord++);
-        }else if( hannoi[0].discos[j]== 3){
-         printf(" %c%c%c%c%c%c%c",bloco, bloco, bloco, bloco, bloco, bloco, bloco);
-          gotoxy(0,++cord);
+    for(j=hannoi[0].top; j>=0; j--){
+        if(hannoi[0].discos[j] == 1){
+           printf("   %c%c%c%c ",caracter, caracter, caracter, caracter);
+           gotoxy(0,cord++);
+        }
+        if(hannoi[0].discos[j] == 2){
+           printf("  %c%c%c%c%c%c",caracter, caracter, caracter, caracter, caracter, caracter);
+           gotoxy(0,cord++);
+        }
+        if(hannoi[0].discos[j] == 3){
+           printf(" %c%c%c%c%c%c%c%c",caracter, caracter, caracter, caracter, caracter, caracter, caracter, caracter);
+           gotoxy(0,++cord);
         }
     }
 
-    gotoxy(25,2);
-
-   printf("PILHA 2");
-   cord=4;
-   gotoxy(25,cord++);
-
-      for (j=hannoi[1].top; j>=0; j--){
-
+    gotoxy(25,2);printf("  Pilha 2");
+    cord=4;
+    gotoxy(25,cord++);
+    for(j=hannoi[1].top; j>=0; j--){
         if(hannoi[1].discos[j]== 1){
-          printf("   %c%c%c    ",bloco, bloco, bloco);
+           printf("   %c%c%c%c    ",caracter, caracter, caracter,caracter);
+           gotoxy(25,cord++);
+        }
+        if(hannoi[1].discos[j]== 2){
+            printf("  %c%c%c%c%c%c",caracter, caracter, caracter, caracter, caracter, caracter);
             gotoxy(25,cord++);
-        }else if( hannoi[1].discos[j]== 2){
-            printf("  %c%c%c%c%c",bloco, bloco, bloco, bloco, bloco);
-              gotoxy(25,cord++);
-        }else if ( hannoi[1].discos[j]== 3){
-         printf(" %c%c%c%c%c%c%c ",bloco, bloco, bloco, bloco, bloco, bloco, bloco);
-           gotoxy(25,++cord);
+        }
+        if(hannoi[1].discos[j]== 3){
+            printf(" %c%c%c%c%c%c%c%c ",caracter, caracter, caracter, caracter, caracter, caracter, caracter, caracter);
+            gotoxy(25,++cord);
         }
       }
 
-      gotoxy(50,2);
-
-      printf("PILHA 3 ");
+      gotoxy(50,2);printf("  Pilha 3");
       cord=4;
-        gotoxy(50,cord++);
+      gotoxy(50,cord++);
       for (j=hannoi[2].top; j>=0; j--){
-
-        if (hannoi[2].discos[j]== 1){
-            printf("   %c%c%c ",bloco, bloco, bloco);
-              gotoxy(50,cord++);
-        }else
-
-        if( hannoi[2].discos[j]== 2){
-            printf("  %c%c%c%c%c  ",bloco, bloco, bloco, bloco, bloco);
-              gotoxy(50,cord++);
-        }else
-
-        if ( hannoi[2].discos[j]== 3){
-            printf(" %c%c%c%c%c%c%c ",bloco, bloco, bloco, bloco, bloco, bloco, bloco);
-              gotoxy(50,++cord);
+        if(hannoi[2].discos[j]== 1){
+            printf("   %c%c%c%c ",caracter, caracter, caracter,caracter);
+            gotoxy(50,cord++);
+        }
+        if(hannoi[2].discos[j]== 2){
+            printf("  %c%c%c%c%c%c  ",caracter, caracter, caracter, caracter, caracter, caracter);
+            gotoxy(50,cord++);
+        }
+        if(hannoi[2].discos[j]== 3){
+            printf(" %c%c%c%c%c%c%c%c ",caracter, caracter, caracter, caracter, caracter, caracter, caracter, caracter);
+            gotoxy(50,++cord);
         }
 
      }
+     gotoxy(0,8.5);printf("*****************************************************************************************************");
+     gotoxy(50,9.8);printf(" Quantidade Minima de Jogadas = 7");
 
   }
 
 int main(void){
-        system("color f0");
-        int i = 0,jogadas = 0;
-        int pilhaRetira = 0,pilhaInsere = 0;
-
-              //Inicializa as pilhas
-              for(i=0;i<3;i++){
-
-                     inicializa(hannoi+i);
-              }
-              //Coloca os discos na primeira pilha
-              start(hannoi);
-        while(end(hannoi)!=1){
-
-
+        system("color 0A");
+        int i = 0,jogadas = 0, pilhaRetira = 0,pilhaInsere = 0;
+              //Iniciando as pilhas
+              for(i=0;i<3;i++)
+                 inicializa(hannoi+i);
+              //Colocando os discos na 1° pilha
+              inicio(hannoi);
+        while(fim(hannoi)!=1){
           imprime(hannoi);
-          gotoxy(0,10);
-          printf("INFORME SUA JOGADA:");
-            gotoxy(0,11);
-          printf("Disco da pilha:");
+          gotoxy(0,9.8);printf("| Quantidade de Jogadas Realizadas = %d ", jogadas);
+          gotoxy(30,10);printf("\n*****************************************************************************************************");
+          gotoxy(20,12);printf("Mover o Disco da Pilha: ");
+          limpaBuffer();
           scanf("%d",&pilhaRetira);
-
           gotoxy(0,13);
-          if( (pilhaRetira!=1)&&(pilhaRetira!=2)&&(pilhaRetira!=3) ){
 
-             printf("jogada Invalida: Esta pilha não existe!");
-             system("pause");
-             system("cls");
+          if((pilhaRetira!=1)&&(pilhaRetira!=2)&&(pilhaRetira!=3)){
+             printf("Jogada Invalida: Esta pilha nao existe!");
+             pausa();
+             limpaTela();
              continue;
            }
-          printf("para a pilha:");
+          gotoxy(20,13);printf("Mover Para a Pilha: ");
+          limpaBuffer();
           scanf("%d",&pilhaInsere);
 
-          if( (pilhaInsere!=1)&&(pilhaInsere!=2)&&(pilhaInsere!=3) ){
-             printf("jogada Invalida: Esta pilha não existe! \n");
-             system("pause");
-             system("cls");
+          if((pilhaInsere!=1)&&(pilhaInsere!=2)&&(pilhaInsere!=3) ){
+             printf("jogada Invalida: Esta pilha nao existe! \n");
+             pausa();
+             limpaTela();
              continue;
            }
+
           jogadas++;
 
-          if ( empty(hannoi[pilhaRetira-1]) ) {
-
-              printf("Jogada Invalida: Pilha Vazia");
-              system("pause");
+          if(empilha(hannoi[pilhaRetira-1])){
+              printf("Jogada Invalida: Pilha Vazia\n");
+              pausa();
 
           }else if(underflow(hannoi[pilhaInsere-1])){
-
              printf("Jogada Invalida: Pilha cheia!!! \n");
-             system("pause");
+             pausa();
 
           }else if(hannoi[pilhaInsere-1].top == -1){
+             push(hannoi+(pilhaInsere-1),pop( hannoi+(pilhaRetira-1)));
 
-            push(hannoi+(pilhaInsere-1),pop( hannoi+(pilhaRetira-1) ) );
+          }else if(hannoi[pilhaRetira-1].discos[hannoi[pilhaRetira-1].top]  > hannoi[pilhaInsere-1].discos[ hannoi[pilhaInsere-1].top]){
+             printf("Jogada Invalida: Disco maior que o topo \n");
+             pausa();
 
-          }else if (hannoi[pilhaRetira-1].discos[  hannoi[pilhaRetira-1].top]  > hannoi[pilhaInsere-1].discos[ hannoi[pilhaInsere-1].top ] ) {
+          }else
+              push(hannoi+(pilhaInsere-1),pop( hannoi+(pilhaRetira-1)));
 
-                   printf("Jogada Invalida: Disco maior que o topo \n");
-
-                 system("pause");
-
-          }else{
-
-              push(hannoi+(pilhaInsere-1),pop( hannoi+(pilhaRetira-1)  ) );
-
-           }
-
-        system("cls");
+        limpaTela();
         }
-        printf("FIM DE JOGO \n");
+        gotoxy(20,10);printf("FIM DE JOGO \n");
 
-        if(jogadas==7){
-          printf("   Parabens!!! \n");
-        }else if( (jogadas>7) && (jogadas<=10) ){
-          printf("   Muito Bom!!! \n");
-        }else if (jogadas > 10){
-          printf("   Precisa Melhorar!!! \n");
+        if(jogadas == 7){
+          gotoxy(20,11);printf("Parabens! Muito Bom!");
         }
-          printf("Voce realizou %d jogadas",jogadas);
+        if((jogadas > 7) && (jogadas <= 10) ){
+          gotoxy(20,11);printf("Bom!");
+        }
+        if(jogadas > 10){
+          gotoxy(20,11);printf("Razoavel!");
+        }
+        gotoxy(20,12);printf("Voce realizou %d jogadas\n",jogadas);
         return 0;
-        system("pause");
-
-}
+        pausa();
+   }
